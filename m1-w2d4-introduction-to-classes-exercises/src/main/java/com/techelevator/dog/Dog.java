fb7bda998a7bf918a5c1d7e2482f512da003b9f8
Heather Rusnica <heather.rusnica@gmail.com>
2017-09-08 14:00:27 -0400

    done

--- m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/dog/Dog.java
+++ m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/dog/Dog.java
@@ -1,9 +1,28 @@
 package com.techelevator.dog;
 
 public class Dog {
-
-	//
-	// Write code here
-	//
+	private boolean sleeping;
 	
+	public String makeSound(){
+		if (sleeping){
+			return "Zzzzz...";
+		} return "Woof!";
+	}
+	
+	public void sleep(){
+		this.sleeping = true;
+	}
+	
+	public void wakeUp(){
+		this.sleeping = false;
+	}
+
+	public boolean isSleeping() {
+		return sleeping;
+	}
+
+	public void setSleeping(boolean sleeping) {
+		this.sleeping = !sleeping;
+	}
+
 }
--- m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/person/Person.java
+++ m1-w2d4-introduction-to-classes-exercises/src/main/java/com/techelevator/person/Person.java
@@ -2,8 +2,43 @@ package com.techelevator.person;
 
 public class Person {
 
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	public String getFullName(){
+	return lastName + ", " + firstName;
+	}
+	
+	public boolean isAdult(){
+		return (age > 17);
+	}
 	//
 	// Write code here
 	//
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
 }
