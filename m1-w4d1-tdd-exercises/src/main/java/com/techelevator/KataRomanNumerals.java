9f225d9e6f7e13c1352767ed00834ef38712ec09
Heather Rusnica <heather.rusnica@gmail.com>
2017-09-18 19:02:37 -0400

    finished

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataRomanNumerals.java
@@ -1,5 +1,120 @@
 package com.techelevator;
 
-public class KataRomanNumerals {
+import java.lang.reflect.Array;
 
-}
+public class KataRomanNumerals {
+public String ones;
+public String tens;
+public String hundreds;
+public String thousands;
+public int input;
+public int[] numArray;
+	
+	
+
+	public int[] convertToNumArray(int input) {
+		String numString =  Integer.toString(input);
+		char[] charArray = numString.toCharArray();
+		int[] numArray = new int[charArray.length];
+		for(int i = 0; i < charArray.length; i++){
+			numArray[i] = Character.getNumericValue(charArray[i]);
+		}
+	return numArray;
+	}
+	
+	public String findValues(int input){
+		numArray = convertToNumArray(input);
+		if(numArray.length == 4){
+			return convertThousands(numArray[0]) + convertHundreds(numArray[1]) + convertTens(numArray[2]) + convertOnes(numArray[3]);
+		}
+		if(numArray.length > 2){
+			return convertHundreds(numArray[0]) + convertTens(numArray[1]) + convertOnes(numArray[2]);
+		}
+		if(numArray.length > 1){
+			return convertTens(numArray[0]) + convertOnes(numArray[1]);
+		}
+		if(numArray.length != 0){
+			return convertOnes(numArray[0]);
+		}
+		return null;
+	}
+	
+	public String convertOnes(int input) {
+		String ones = "";
+		if(input == 0){
+			return "";
+		}else if(input < 4){
+			for(int i = 0; i < input; i++){
+			ones += "I";
+			}
+		} else if (input == 4){
+			ones = "IV";
+		} else if(input == 5){
+			ones = "V";
+		} else if(input > 5 && input <= 8){
+			ones = "V";
+			for(int i = 6; i <= input; i++){
+			ones += "I";
+			}
+		} if(input == 9){
+			ones = "IX";
+		}
+		return ones;
+		}
+	
+	public String convertTens(int input) {
+		String tens = "";
+			if(input == 0){
+				return "";
+			} else if(input < 4){
+			for(int i = 0; i < input; i++){
+			tens += "X";
+			}
+		} else if (input == 4){
+			tens = "XL";
+		} else if(input == 5){
+			tens = "L";
+		} else if(input > 5 && input <= 8){
+			tens = "L";
+			for(int i = 6; i <= input; i++){
+			tens += "X";
+			}
+		} if(input == 9){
+			tens = "XC";
+		}
+		return tens;
+		}
+	
+	public String convertHundreds(int input) {
+		String hundreds = "";
+			if(input == 0){
+				return "";
+			} else if(input < 4){
+			for(int i = 0; i < input; i++){
+			hundreds += "C";
+			}
+		} else if (input == 4){
+			hundreds = "CD";
+		} else if(input == 5){
+			hundreds = "D";
+		} else if(input > 5 && input <= 8){
+			hundreds = "D";
+			for(int i = 6; i <= input; i++){
+			hundreds += "C";
+			}
+		} if(input == 9){
+			hundreds = "CM";
+		}
+		return hundreds;
+		}
+	
+	public String convertThousands(int input) {
+		String thousands = "";
+			for(int i = 0; i < input; i++){
+			thousands += "M";
+			}
+		return thousands;
+		}
+	}
+	
+
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataRomanNumeralsTest.java
@@ -5,4 +5,71 @@ import static org.junit.Assert.*;
 
 public class KataRomanNumeralsTest {
 
+KataRomanNumerals sut;
+
+	@Before
+	public void setUp(){
+	sut = new KataRomanNumerals();
+	}
+	
+	@Test
+public void testConvertOne(){
+		assertEquals("IV", sut.convertOnes(4));
+	}
+
+	@Test
+	public void testConvertFive(){
+			assertEquals("V", sut.convertOnes(5));
+		}
+	
+	@Test
+	public void testConvertEight(){
+			assertEquals("VIII", sut.convertOnes(8));
+		}
+	
+	@Test
+	public void testConvertNine(){
+			assertEquals("IX", sut.convertOnes(9));
+		}
+	@Test
+	public void testConvertToArray(){
+			assertArrayEquals(new int[] {1, 0}, sut.convertToNumArray(10));
+		}
+	
+	@Test
+	public void testFindValues(){
+			assertEquals("I", sut.findValues(1));
+}
+	@Test
+	public void testConver30(){
+			assertEquals("XXX", sut.convertTens(3));
+		}
+	
+	@Test
+	public void testConvert500(){
+		assertEquals("D", sut.convertHundreds(5));
+	}
+	
+	@Test
+	public void testConvert2000(){
+		assertEquals("MM", sut.convertThousands(2));
+	}
+	
+	@Test
+	public void testEverything(){
+		assertEquals("CXI", sut.findValues(111));
+	}
+	
+	@Test
+	public void testEverything100(){
+		assertEquals("C", sut.findValues(100));
+	}
+	
+	@Test
+	public void testEverything1537(){
+		assertEquals("MDXXXVII", sut.findValues(1537));
+	}
+	
+	
+	
 }
