3798d5d2595daa0b0142d5f74c55434a724c50b1
Heather Rusnica <heather.rusnica@gmail.com>
2017-11-01 15:47:30 -0400

    shipping

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -1,5 +1,18 @@
 ﻿/// <reference path="../jquery-3.1.1.js" />
-
 $(document).ready(function () {
+
+	$('#SameShipping').on('click', function() {
+		$('#ShippingAddress1').val($('#BillingAddress1').val());
+		$('#ShippingAddress2').val($('#BillingAddress2').val());
+		$('#ShippingCity').val($('#BillingCity').val());
+		$('#ShippingState').val($('#BillingState').val());
+		$('#ShippingPostalCode').val($('#BillingPostalCode').val());
+	});
+	
+	$('input[name="ShippingType"]').on('click', function() {
+		$('#shipping > .price').text('$' + $(this).attr('data-cost'));
+		var subtotal = $('#subtotal > .price').text().substring(1);
+		$('#grandtotal > .price').text('$' + (parseFloat($(this).attr('data-cost'))+parseFloat(subtotal)));
+	});
 
 });
